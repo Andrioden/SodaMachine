47d0e8e2b4beb9faadfe3d80fd2379eaa2215ca5
André S. Hansen <andre.ok@online.no>
2019-11-21 07:05:31 +0100

    Improvements.txt

---
+++ SodaSystems.Tests/SodaMachineTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SodaSystems.Console;
+using SodaSystems.Core;
+
+namespace SodaSystems.Tests
+{
+    [TestClass]
+    public class SodaMachineTests
+    {
+        //public void UT_SodaMachine_todo()
+        //{
+
+        //}
+    }
+}
